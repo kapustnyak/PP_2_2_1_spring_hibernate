b4e5a70c3fe2cec7efe574471b420b89a6f236a1
kapustnyak <digitex73@yandex.ru>
2025-03-28 16:28:57 +0300

    Конструктор

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,18 +16,14 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      Car car1 = new Car("Cadillac", 1);
-      Car car2 = new Car("Mustang", 2);
-      Car car3 = new Car("Camaro", 3);
-      Car car4 = new Car("Toyota", 4);
-
-
-
-
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",
+              new Car("Cadillac", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",
+              new Car("Mustang", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",
+              new Car("Camaro", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",
+              new Car("Toyota", 4)));
 
 
 
